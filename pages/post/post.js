8cf9201379f9d69376d2edff40155b5c9fd0595a
ZIMUQIN-L <68744060+ZIMUQIN-L@users.noreply.github.com>
2024-02-26 16:10:12 +0100

    [Feat]: add post

--- pages/post/post.js
+++ pages/post/post.js
@@ -91,4 +91,40 @@ Page({
   onNumberChange: function(e) {
     console.log(e.detail);
   },
+  onGoHome() {
+    wx.navigateTo({
+      url: '/pages/home/home',
+      success: () => {},
+      error: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '打开个人中心失败',
+        });
+      },
+    });
+  },
+  onGoProfile() {
+    wx.navigateTo({
+      url: '/pages/user/index',
+      success: () => {},
+      error: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '打开个人中心失败',
+        });
+      },
+    });
+  },
+  onGoPost() {
+    wx.navigateTo({
+      url: '/pages/post/post',
+      success: () => {},
+      error: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '打开个人中心失败',
+        });
+      },
+    });
+  },
 });
--- pages/search/search.js
+++ pages/search/search.js
@@ -110,6 +110,42 @@ Page({
       icon: 'none',
     });
   },
+  onGoHome() {
+    wx.navigateTo({
+      url: '/pages/home/home',
+      success: () => {},
+      error: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '打开个人中心失败',
+        });
+      },
+    });
+  },
+  onGoProfile() {
+    wx.navigateTo({
+      url: '/pages/user/index',
+      success: () => {},
+      error: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '打开个人中心失败',
+        });
+      },
+    });
+  },
+  onGoPost() {
+    wx.navigateTo({
+      url: '/pages/post/post',
+      success: () => {},
+      error: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '打开个人中心失败',
+        });
+      },
+    });
+  },
 
   sorry() {
     wx.showToast({
